54e1f190ae0393d72b3432965aad796b88aba496
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-06-17 11:51:26 +0200

    chore: log CORS

--- src/index.ts
+++ src/index.ts
@@ -54,6 +54,8 @@ app.use(
         return origin;
       }
 
+      console.log('Origin not allowed:', origin);
+
       return origin.endsWith('egdata.app') ? origin : 'https://egdata.app';
     },
     allowHeaders: ['Content-Type', 'Authorization'],
