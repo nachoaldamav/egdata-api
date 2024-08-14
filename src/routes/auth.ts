9f552d5a3f7f6e494fc0d4649db623bd5fdb165b
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-08-14 23:02:27 +0200

    chore: disallow every domain for auth routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
 /**
  * This route handles the authentication with Epic Games.
  */
 const app = new Hono();
+
+app.use(cors());
 
 app.get('/', (c) => {
   return c.json({ message: 'Hello, World!' });
