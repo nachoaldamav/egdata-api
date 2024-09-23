607ff5bc34cd1c03c71fd6c535f69664f704c92a
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-09-23 18:15:05 +0200

    fix: tokens refresh

--- src/trigger/refresh-tokens.ts
+++ src/trigger/refresh-tokens.ts
@@ -1,26 +1,33 @@
-import { logger, schedules } from '@trigger.dev/sdk/v3';
+import { envvars, logger, schedules } from '@trigger.dev/sdk/v3';
 import axios from 'axios';
 
 export const refreshTokens = schedules.task({
   id: 'refresh-tokens',
   cron: '*/10 * * * *',
   run: async (payload, { ctx }) => {
-    logger.log('Refreshing Epic Games tokens');
-    const res = await axios.patch<{
-      status: string;
-    }>('https://api.egdata.app/auth/refresh', {
+    const token = await envvars.retrieve('JWT_SECRET');
+
+    if (!token) {
+      logger.error('Missing JWT_SECRET env variable');
+      throw new Error('Missing JWT_SECRET env variable');
+    }
+
+    logger.log('Refreshing Epic Games tokens', {
+      token: `${token.value.substring(0, 5)}...`,
+    });
+
+    const res = await fetch('https://api.egdata.app/auth/refresh', {
+      method: 'PATCH',
       headers: {
-        Authorization: `Bearer ${process.env.JWT_SECRET}`,
+        Authorization: `Bearer ${token.value}`,
       },
     });
 
-    logger.debug(JSON.stringify(res.data));
-
-    if (res.status === 200) {
-      logger.log('Epic Games tokens refreshed');
-    } else {
-      logger.error('Error refreshing Epic Games tokens', res.data);
+    if (!res.ok) {
+      logger.error('Error refreshing Epic Games tokens', await res.json());
       throw new Error('Error refreshing Epic Games tokens');
     }
+
+    logger.log('Epic Games tokens refreshed');
   },
 });
