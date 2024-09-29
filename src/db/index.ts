bcabcae16451da54b692a2dbb7c499230494bf39
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-09-29 21:00:07 +0200

    chore: check if certs exists

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,20 @@
 import mongoose from 'mongoose';
-import { existsSync, readdirSync } from 'node:fs';
+import { existsSync, readdirSync, lstatSync } from 'node:fs';
 import { dirname } from 'node:path';
+
+/**
+ * This function first check if the file is a directory, then if it is a file, and then if it exists.
+ */
+function checkFile(file: string) {
+  console.log(`Checking file: ${file}`);
+  try {
+    const stat = lstatSync(file);
+    return stat.isFile();
+  } catch (err) {
+    console.error(`Error checking file: ${file}`, err);
+    return false;
+  }
+}
 
 export class DB {
   db: mongoose.Connection;
@@ -10,50 +24,34 @@ export class DB {
 
   async connect() {
     console.log('Connecting to MongoDB', {
-      url: process.env['MONGO_URL'],
-      ca: process.env['MONGO_CA']?.substring(0, 100),
-      cert: process.env['MONGO_CERT']?.substring(0, 100),
+      url: process.env.MONGO_URL,
+      ca: process.env.MONGO_CA?.substring(0, 100),
+      cert: process.env.MONGO_CERT?.substring(0, 100),
     });
 
-    if (!process.env['MONGO_URL']) {
+    if (!process.env.MONGO_URL) {
       throw new Error('MONGO_URL is required');
     }
 
-    if (!process.env['MONGO_CA']) {
+    if (!process.env.MONGO_CA) {
       throw new Error('MONGO_CA is required');
     }
 
-    if (!process.env['MONGO_CERT']) {
+    if (!process.env.MONGO_CERT) {
       throw new Error('MONGO_CERT is required');
     }
 
-    const caExists = existsSync(process.env['MONGO_CA']);
-    const certExists = existsSync(process.env['MONGO_CERT']);
+    checkFile(process.env.MONGO_CA);
+    checkFile(process.env.MONGO_CERT);
 
-    if (!caExists || !certExists) {
-      console.error('CA or CERT file does not exist', {
-        ca: !!caExists,
-        cert: !!certExists,
-      });
-
-      const filesInDir = readdirSync(dirname(process.env['MONGO_CA']));
-
-      console.error('Files in dir', filesInDir);
-
-      throw new Error('CA or CERT file does not exist');
-    }
-
-    await mongoose.connect(
-      `mongodb://${process.env['MONGO_URL']}:27017/egdata`,
-      {
-        authMechanism: 'MONGODB-X509',
-        authSource: '$external',
-        tlsCAFile: process.env['MONGO_CA'],
-        tlsCertificateKeyFile: process.env['MONGO_CERT'],
-        tls: true,
-        tlsAllowInvalidCertificates: true,
-      }
-    );
+    await mongoose.connect(`mongodb://${process.env.MONGO_URL}:27017/egdata`, {
+      authMechanism: 'MONGODB-X509',
+      authSource: '$external',
+      tlsCAFile: process.env.MONGO_CA,
+      tlsCertificateKeyFile: process.env.MONGO_CERT,
+      tls: true,
+      tlsAllowInvalidCertificates: true,
+    });
     console.log('Connected to MongoDB');
   }
 
