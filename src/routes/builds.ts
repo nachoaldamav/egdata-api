c9de38126cfc925282735cba5835d35ddd6f0d82
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-10-26 11:29:36 +0200

    feat: build sizes

--- src/routes/builds.ts
+++ src/routes/builds.ts
@@ -1,30 +1,41 @@
 import { Hono } from 'hono';
 import { db } from '../db/index.js';
 import { Item } from '../db/schemas/item.js';
+import { Asset } from '../db/schemas/assets.js';
+import { ObjectId } from 'mongodb';
 
 const app = new Hono();
 
-app.get('/:hash', async (c) => {
-  const { hash } = c.req.param();
+app.get('/:id', async (c) => {
+  const { id } = c.req.param();
 
   const build = await db.db.collection('builds').findOne({
-    hash,
+    _id: new ObjectId(id),
   });
 
   if (!build) {
     return c.json({ error: 'Build not found' }, 404);
   }
 
-  return c.json(build);
+  const asset = await Asset.findOne({
+    artifactId: build.appName,
+    platform: build.labelName.split('-')[1],
+  });
+
+  return c.json({
+    ...build,
+    downloadSizeBytes: asset?.downloadSizeBytes,
+    installedSizeBytes: asset?.installedSizeBytes,
+  });
 });
 
-app.get('/:hash/files', async (c) => {
-  const { hash } = c.req.param();
+app.get('/:id/files', async (c) => {
+  const { id } = c.req.param();
   const limit = Number.parseInt(c.req.query('limit') || '25', 10);
   const page = Number.parseInt(c.req.query('page') || '1', 10);
 
   const build = await db.db.collection('builds').findOne({
-    hash,
+    _id: new ObjectId(id),
   });
 
   if (!build) {
@@ -34,7 +45,7 @@ app.get('/:hash/files', async (c) => {
   const files = await db.db
     .collection('files')
     .find({
-      manifestHash: hash,
+      manifestHash: build.hash,
     })
     .sort({ depth: 1, filename: 1 })
     .skip((page - 1) * limit)
@@ -46,7 +57,7 @@ app.get('/:hash/files', async (c) => {
     page,
     limit,
     total: await db.db.collection('files').countDocuments({
-      manifestHash: hash,
+      manifestHash: build.hash,
     }),
   });
 });
