aeafbf5f65d29bf5a6ad81d66826dce5e748f0ad
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-12-26 19:02:42 +0100

    feat: user service API

--- src/routes/users-service.ts
+++ src/routes/users-service.ts
@@ -1,24 +1,19 @@
 import { Hono } from "hono";
-import { createMiddleware } from 'hono/factory'
-import jwt from 'jsonwebtoken';
+import { createMiddleware } from "hono/factory";
+import jwt from "jsonwebtoken";
+import { db } from "../db/index.js";
+
+interface Playtime {
+  accountId: string;
+  artifactId: string;
+  totalTime: number;
+}
 
 const app = new Hono();
 
-const middleware = createMiddleware(async (c, next) => {
-    // Check if the user is authenticated
-    if (!c.req.header('Authorization')) {
-      return c.json({ error: 'Unauthorized' }, 401);
-    }
-
-    const authorization = c.req.header('Authorization');
-
-    if (!authorization || !authorization.startsWith('Bearer ')) {
-      return c.json({ error: 'Invalid authorization header' }, 401);
-    }
-
-    const token = authorization.replace('Bearer ', '').replace('eg1~', '');
-
-    const decoded = jwt.decode(token) as {
+const middleware = createMiddleware<{
+  Variables: {
+    jwt: {
       sub: string;
       iss: string;
       dn: string;
@@ -33,21 +28,90 @@ const middleware = createMiddleware(async (c, next) => {
       iat: number;
       jti: string;
     };
+  };
+}>(async (c, next) => {
+  // Check if the user is authenticated
+  if (!c.req.header("Authorization")) {
+    return c.json({ error: "Unauthorized" }, 401);
+  }
 
-    if (!decoded || !decoded.sub) {
-      console.error('Invalid JWT');
-      return c.json({ error: 'Invalid JWT' }, 401);
-    }
+  const authorization = c.req.header("Authorization");
 
-    console.log('JWT decoded', decoded);
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return c.json({ error: "Invalid authorization header" }, 401);
+  }
 
-    await next()
-  })
+  const token = authorization.replace("Bearer ", "").replace("eg1~", "");
 
-app.use(middleware);
+  const decoded = jwt.decode(token) as {
+    sub: string;
+    iss: string;
+    dn: string;
+    nonce: string;
+    pfpid: string;
+    sec: number;
+    aud: string;
+    t: string;
+    scope: string;
+    appid: string;
+    exp: number;
+    iat: number;
+    jti: string;
+  };
 
-app.get('/', async (c) => {
-  return c.json({ message: 'Hello, World!' });
+  if (!decoded || !decoded.sub) {
+    console.error("Invalid JWT");
+    return c.json({ error: "Invalid JWT" }, 401);
+  }
+
+  // Check if it's expired
+  const expirationDate = new Date(decoded.exp * 1000);
+
+  if (expirationDate < new Date()) {
+    console.error("JWT is expired");
+    return c.json({ error: "JWT is expired" }, 401);
+  }
+
+  c.set("jwt", decoded);
+
+  await next();
+});
+
+app.get("/", middleware, async (c) => {
+  return c.json({ message: "Hello, World!" });
+});
+
+app.post("/playtime", middleware, async (c) => {
+  const body = await c.req.json<Playtime[]>();
+
+  const jwt = c.var.jwt;
+
+  const id = jwt.sub;
+
+  const correctEntries = body.filter(
+    (p) => p.accountId === id && p.artifactId
+  );
+
+  await Promise.all(
+    correctEntries.map(async (p) => {
+      await db.db.collection("playtime").updateOne(
+        {
+          accountId: p.accountId,
+          artifactId: p.artifactId,
+        },
+        {
+          $set: {
+            totalTime: p.totalTime,
+          },
+        },
+        {
+          upsert: true,
+        }
+      );
+    })
+  );
+
+  return c.json({ message: "ok" });
 });
 
 export default app;
