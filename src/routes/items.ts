e3f64fa7d2077af9bd0b59580c4f1f8b27e42a38
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2025-01-09 19:32:35 +0100

    feat: items changelog

--- src/routes/items.ts
+++ src/routes/items.ts
@@ -3,6 +3,9 @@ import { Item } from '@egdata/core.schemas.items';
 import { attributesToObject } from '../utils/attributes-to-object.js';
 import { Asset } from '@egdata/core.schemas.assets';
 import { db } from '../db/index.js';
+import { Changelog } from '@egdata/core.schemas.changelog';
+import { ObjectId } from 'mongodb';
+import client from '../clients/redis.js';
 
 const app = new Hono();
 
@@ -127,6 +130,149 @@ app.get('/:id/builds', async (c) => {
   );
 
   return c.json(assets);
+});
+
+app.get("/:id/changelog", async (c) => {
+  const { id } = c.req.param();
+
+  const limit = Math.min(Number.parseInt(c.req.query("limit") || "10"), 50);
+  const page = Math.max(Number.parseInt(c.req.query("page") || "1"), 1);
+  const skip = (page - 1) * limit;
+
+  const cacheKey = `changelog:${id}:${page}:${limit}`;
+  const cached = await client.get(cacheKey);
+
+  if (cached) {
+    return c.json(JSON.parse(cached), 200, {
+      "Cache-Control": "public, max-age=60",
+    });
+  }
+
+  const item = await Item.findOne({
+    id,
+  });
+
+  if (!item) {
+    c.status(404);
+    return c.json({
+      message: "Item not found",
+    });
+  }
+
+
+  const assets = await Asset.find({
+    itemId: item.id,
+  });
+
+  const builds = await db.db
+    .collection<{
+      appName: string;
+      labelName: string;
+      buildVersion: string;
+      hash: string;
+      metadata: {
+        installationPoolId: string;
+      };
+      createdAt: {
+        $date: string;
+      };
+      updatedAt: {
+        $date: string;
+      };
+      technologies: Array<{
+        section: string;
+        technology: string;
+      }>;
+      downloadSizeBytes: number;
+      installedSizeBytes: number;
+    }>("builds")
+    .find({
+      appName: { $in: assets.map((a) => a.artifactId) },
+    })
+    .toArray();
+
+  const allIds = [
+    id,
+    ...assets.map((a) => a.artifactId).concat(builds.map((b) => b._id.toString())),
+  ];
+
+  const changelist = await Changelog.find(
+    {
+      "metadata.contextId": { $in: allIds },
+    },
+    undefined,
+    {
+      sort: {
+        timestamp: -1,
+      },
+      limit,
+      skip,
+    }
+  );
+
+  const changelistWithDocuments = await Promise.all(
+    changelist.map(async (changelist) => {
+      if (changelist.metadata.contextType === "item") {
+        return {
+          ...changelist.toJSON(),
+          document: await Item.findOne({ id: changelist.metadata.contextId }).exec(),
+        };
+      }
+
+      if (changelist.metadata.contextType === "asset") {
+        return {
+          ...changelist.toJSON(),
+          document: await Asset.findOne({ id: changelist.metadata.contextId }).exec(),
+        };
+      }
+
+      if (changelist.metadata.contextType === "build") {
+        return {
+          ...changelist.toJSON(),
+          document: await db.db
+            .collection<{
+              appName: string;
+              labelName: string;
+              buildVersion: string;
+              hash: string;
+              metadata: {
+                installationPoolId: string;
+              };
+              createdAt: {
+                $date: string;
+              };
+              updatedAt: {
+                $date: string;
+              };
+              technologies: Array<{
+                section: string;
+                technology: string;
+              }>;
+              downloadSizeBytes: number;
+              installedSizeBytes: number;
+            }>("builds")
+            .findOne({ _id: new ObjectId(changelist.metadata.contextId) }),
+        };
+      }
+
+      return {
+        ...changelist.toJSON(),
+      };
+    })
+  );
+
+  if (!changelistWithDocuments) {
+    c.status(404);
+    return c.json({
+      message: "Changelist not found",
+    });
+  }
+
+  await client.set(cacheKey, JSON.stringify(changelistWithDocuments), {
+    EX: 60,
+  });
+
+  return c.json(changelistWithDocuments);
 });
 
 export default app;
