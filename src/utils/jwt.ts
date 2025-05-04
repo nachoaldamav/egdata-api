3241dcbfab6e85bfef71ecea560abcaf736a517b
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2025-05-04 22:44:15 +0200

    chore: remove check

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,10 +5,6 @@ const { sign, verify } = jwt;
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
 
-if (!JWT_SECRET) {
-  throw new Error("Missing environment variable: JWT_SECRET");
-}
-
 export function generateJWT(payload: object): string {
   return sign(payload, JWT_SECRET, {
     expiresIn: "365d",
